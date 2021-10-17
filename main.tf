120ad6fbeadc2d9a8d66df867960beef67705a88
Victor Roest <victor@xirion.net>
2021-10-17 12:04:23 +0200

    added: NixOS Template LXC

--- main.tf
+++ main.tf
@@ -9,4 +9,31 @@ terraform {
 
 provider "proxmox" {
   pm_api_url = "https://10.42.42.42:8006/api2/json"
+  pm_tls_insecure = true
 }
+
+# For full info see: https://blog.xirion.net/posts/nixos-proxmox-lxc/
+resource "proxmox_lxc" "nixos-template" {
+  target_node = "nuc"
+  description = "NixOS LXC Template"
+  hostname = "nixos-template"
+  ostemplate = "local:vztmpl/nixos-unstable-default_156198829_amd64.tar.xz"
+  ostype = "unmanaged"
+  unprivileged = true
+
+  features {
+    nesting = true
+  }
+
+  rootfs {
+    storage = "local-zfs"
+    size = "8G"
+  }
+
+  network {
+    name = "eth0"
+    bridge = "vmbr0"
+    ip = "dhcp"
+  }
+}
+
