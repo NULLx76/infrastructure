69afb7a9c36e1aa98835d5832ec119f48340408f
Victor <victor@xirion.net>
2022-05-06 18:31:03 +0200

    update bootstrap url

--- flux/bootstrap.sh
+++ flux/bootstrap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-flux bootstrap git --url=ssh://git@git.xirion.net:2222/0x76/infrastructure.git --branch=main --path=flux/cluster/base/ --ssh-key-algorithm=ed25519 --components-extra=image-reflector-controller,image-automation-controller
+flux bootstrap git --url=ssh://gitea@git.0x76.dev:42/v/infrastructure.git --branch=main --path=flux/cluster/base/ --ssh-key-algorithm=ed25519 --components-extra=image-reflector-controller,image-automation-controller
