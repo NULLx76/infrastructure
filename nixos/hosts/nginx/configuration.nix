73ce586206d074378027290eb2106487abb9ce6c
Victor <victor@xirion.net>
2022-05-16 14:17:45 +0200

    nginx: brotli

--- nixos/hosts/nginx/configuration.nix
+++ nixos/hosts/nginx/configuration.nix
@@ -51,6 +51,17 @@ in
 
   };
 
+  services.nginx.commonHttpConfig = ''
+    brotli on;
+    brotli_comp_level 6;
+    brotli_static on;
+    brotli_types application/atom+xml application/javascript application/json application/rss+xml
+                application/vnd.ms-fontobject application/x-font-opentype application/x-font-truetype
+                application/x-font-ttf application/x-javascript application/xhtml+xml application/xml
+                font/eot font/opentype font/otf font/truetype image/svg+xml image/vnd.microsoft.icon
+                image/x-icon image/x-win-bitmap text/css text/javascript text/plain text/xml;
+  '';
+
   security.acme.defaults.email = "victorheld12@gmail.com";
   security.acme.acceptTerms = true;
   security.acme.preliminarySelfsigned = true;
