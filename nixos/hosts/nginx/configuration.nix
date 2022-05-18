e07f7bf6ab8af9276c927c684b19eed31ff357c5
Victor <victor@xirion.net>
2022-05-18 12:05:23 +0200

    authentik: deploy

--- nixos/hosts/nginx/configuration.nix
+++ nixos/hosts/nginx/configuration.nix
@@ -45,6 +45,7 @@ in
 
     # Kubernetes endpoints
     virtualHosts."0x76.dev" = k8s_proxy;
+    virtualHosts."id.0x76.dev" = k8s_proxy;
     virtualHosts."zookeeper.0x76.dev" = k8s_proxy;
     virtualHosts."wooloofan.club" = k8s_proxy;
     virtualHosts."whoami.wooloofan.club" = k8s_proxy;
