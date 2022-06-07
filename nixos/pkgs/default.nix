da6bc519eee9f12ff7d25208f705d7e548049ecf
Victor <victor@xirion.net>
2022-06-07 23:57:57 +0200

    manually upgrade renovate

--- nixos/pkgs/default.nix
+++ nixos/pkgs/default.nix
@@ -1,6 +1,11 @@
 final: prev: {
-  # clickhouse = prev.callPackage ./clickhouse { };
-  
+  hedgedoc = prev.hedgedoc.overrideAttrs (old: {
+    # see https://github.com/NixOS/nixpkgs/issues/176127#issuecomment-1146782555
+    preBuild = ''
+      export HOME=$TMPDIR
+    '';
+  });
+
   v = {
     unbound = prev.unbound.override {
       withSystemd = true;
