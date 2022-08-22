12c415edf7e970a8dc86ae1b0f02ca5f51ccbfa2
victor <victor@xirion.net>
2022-08-22 08:36:08 +0200

    laptop: various updates

--- nixos/hosts/thalassa/null/home.nix
+++ nixos/hosts/thalassa/null/home.nix
@@ -14,8 +14,25 @@
     nixpkgs-review
     ripgrep
     rsync
-    tmux
+    rustup
+    rust-analyzer
+    steam-run
+    texlive.combined.scheme-full
   ];
+
+  programs.git = {
+    enable = true;
+    package = pkgs.gitAndTools.gitFull;
+    userName = "Victor";
+    userEmail = "victor@xirion.net";
+  };
+
+  programs.tmux = {
+    enable = true;
+    shortcut = "b";
+    terminal = "screen-256color";
+    clock24 = true;
+  };
 
   programs.home-manager.enable = true;
   programs.firefox = {
@@ -24,7 +41,13 @@
   };
   programs.vscode = {
     enable = true;
-    package = pkgs.vscode.fhs;
+    package = pkgs.vscode;
+    extensions = with pkgs.vscode-extensions; [
+      matklad.rust-analyzer
+      jnoortheen.nix-ide
+      james-yu.latex-workshop
+      valentjn.vscode-ltex
+    ];
   };
 
   gtk = {
