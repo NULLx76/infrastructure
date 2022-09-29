7eb07ac622517c3ed0ccad667c9facb9c46e160b
victor <victor@xirion.net>
2022-09-29 20:52:21 +0200

    fixed up vault config

--- nixos/hosts/olympus/vault-0/configuration.nix
+++ nixos/hosts/olympus/vault-0/configuration.nix
@@ -2,12 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, hosts, ... }:
-let
-  port = 8200;
-  clusterPort = 8201;
-  ip = config.deployment.targetHost;
-in {
+{ config, pkgs, hosts, ... }: {
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
--- nixos/hosts/olympus/vault-1/configuration.nix
+++ nixos/hosts/olympus/vault-1/configuration.nix
@@ -2,11 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, hosts, ... }:
-let
-  port = 8200;
-  clusterPort = 8201;
-in {
+{ config, pkgs, hosts, ... }: {
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
@@ -14,9 +10,6 @@ in {
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "21.05"; # Did you read the comment?
-
-  # Vault
-  networking.firewall.allowedTCPPorts = [ port clusterPort ];
 
   # Vault
   services.v.vault = {
