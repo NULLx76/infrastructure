887e408d9a02a3d85fd713c2bc03e09f717b713e
victor <victor@xirion.net>
2022-10-08 12:27:56 +0200

    fixed vscode hash

--- nixos/pkgs/vscode-extensions/platformio.nix
+++ nixos/pkgs/vscode-extensions/platformio.nix
@@ -1,12 +1,10 @@
 { vscode-utils, lib }:
 let inherit (vscode-utils) buildVscodeMarketplaceExtension;
 in buildVscodeMarketplaceExtension {
-  # TODO: Auto install settings: `"platformio-ide.useBuiltinPIOCore": false`
   mktplcRef = {
     name = "platformio-ide";
     publisher = "platformio";
     version = "2.5.4";
-    sha256 = "sha256-/vBdZ6Mu1KlF+glqp5CNt9WeK1ECqfaivCnK8TisChg=";
+    sha256 = "sha256-KbXlegQSLjMCVotR1mU/CDiaQMKLLSX+nnbRJgdFTz8=";
   };
-  # meta = { license = lib.licenses.apache; };
 }
