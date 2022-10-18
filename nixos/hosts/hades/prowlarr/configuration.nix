f11f5ecadadf6d3592a349310cb3add767bd41ed
Victor <victor@xirion.net>
2022-10-18 16:26:36 +0200

    add flaresolverr to prowlarr

--- nixos/hosts/hades/prowlarr/configuration.nix
+++ nixos/hosts/hades/prowlarr/configuration.nix
@@ -7,4 +7,18 @@
     enable = true;
     openFirewall = true;
   };
+  
+  virtualisation.podman.enable = true;
+
+  virtualisation.oci-containers = {
+    backend = "podman";
+    containers = {
+      flaresolverr = {
+        image = "0x76/flaresolverr:3.0.0.beta2-fixup";
+        ports = [
+          "8191:8191"
+        ];
+      };
+    };
+  };
 }
