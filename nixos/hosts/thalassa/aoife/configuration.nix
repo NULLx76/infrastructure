2dd3d2f5ab00be66592e881c576d913b5c9ec041
Victor <victor@xirion.net>
2022-11-29 13:10:17 +0100

    minor fix

--- nixos/hosts/thalassa/aoife/configuration.nix
+++ nixos/hosts/thalassa/aoife/configuration.nix
@@ -100,31 +100,29 @@
 
   virtualisation.podman.enable = true;
 
-  fonts = {
-    fonts = with pkgs; [
-      material-design-icons
-      noto-fonts
-      noto-fonts-cjk
-      noto-fonts-emoji
-      dejavu_fonts
-      (nerdfonts.override {
-        fonts =
-          [ "DejaVuSansMono" "Ubuntu" "DroidSansMono" "NerdFontsSymbolsOnly" ];
-      })
-    ];
+  fonts.fonts = with pkgs; [
+    material-design-icons
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    dejavu_fonts
+    (nerdfonts.override {
+      fonts =
+        [ "DejaVuSansMono" "Ubuntu" "DroidSansMono" "NerdFontsSymbolsOnly" ];
+    })
+  ];
 
-    # Allow unfree packages
-    nixpkgs.config.allowUnfree = true;
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
 
-    # networking.firewall.enable = false;
+  # networking.firewall.enable = false;
 
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It‘s perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-    system.stateVersion = "23.05"; # Did you read the comment?
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
 
-  };
 }
