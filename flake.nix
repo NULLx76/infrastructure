16cdaa2ec72772078b5caf9f6167fcfe2e6a2901
Victor <victor@xirion.net>
2023-02-17 13:18:16 +0100

    fix nixvim

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
     mailserver.url = "gitlab:simple-nixos-mailserver/nixos-mailserver";
     mailserver.inputs.nixpkgs.follows = "nixpkgs";
 
-    nixvim.url = "github:pta2002/nixvim";
+    nixvim.url = "github:GaetanLepage/nixvim/fix-plugins-default";
 
     nixos-generators.url = "github:nix-community/nixos-generators";
 
