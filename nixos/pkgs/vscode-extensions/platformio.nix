9eb2f34fec2c8d6474f2baca9b5850efc17ca8e3
Victor <victor@xirion.net>
2023-04-30 11:14:51 +0200

    update platform io vscode

--- nixos/pkgs/vscode-extensions/platformio.nix
+++ nixos/pkgs/vscode-extensions/platformio.nix
@@ -4,7 +4,8 @@ in buildVscodeMarketplaceExtension {
   mktplcRef = {
     name = "platformio-ide";
     publisher = "platformio";
-    version = "2.5.4";
-    sha256 = "sha256-/vBdZ6Mu1KlF+glqp5CNt9WeK1ECqfaivCnK8TisChg=";
+    version = "3.1.1";
+    sha256 = "sha256-fwEct7Tj8bfTOLRozSZJGWoLzWRSvYz/KxcnfpO8Usg=";
+    # sha256 = lib.fakeSha256;
   };
 }
