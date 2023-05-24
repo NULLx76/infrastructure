f2a1f85df85c58f254d0777c1431903e432ddd90
Victor Roest <victor@xirion.net>
2023-05-24 12:10:54 +0200

    fix deadnix

--- nixos/hosts/hades/immich/configuration.nix
+++ nixos/hosts/hades/immich/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ pkgs, config, lib, ... }:
+{ pkgs, config, ... }:
 let
   # https://github.com/immich-app/immich/releases
   version = "1.55.1";
