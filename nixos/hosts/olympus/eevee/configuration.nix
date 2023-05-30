af8d436204227488418cf3357cd5bf236bf1069a
Victor <victor@xirion.net>
2023-05-30 14:36:52 +0200

    fix deadnix

--- nixos/hosts/olympus/eevee/configuration.nix
+++ nixos/hosts/olympus/eevee/configuration.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ pkgs, inputs, ... }: {
+{ pkgs, ... }: {
   imports = [ ./hardware-configuration.nix ./hardware.nix ];
 
   # Bootloader.
