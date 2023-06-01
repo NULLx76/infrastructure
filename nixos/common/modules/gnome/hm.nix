1e702d8469ad72e2d75af0c717ab012c5896ac0a
Victor <victor@xirion.net>
2023-06-01 12:26:55 +0200

    flake update

--- nixos/common/modules/gnome/hm.nix
+++ nixos/common/modules/gnome/hm.nix
@@ -18,9 +18,20 @@ let
 in {
   xdg.mimeApps.enable = true;
   xdg.mimeApps.defaultApplications = {
-    "text/html" = "firefox.desktop";
     "text/plain" = "org.gnome.TextEditor.desktop";
     "application/pdf" = "org.gnome.Evince.desktop";
+
+    # Firefox
+    "text/html" = "firefox.desktop";
+    "x-scheme-handler/http" = "firefox.desktop";
+    "x-scheme-handler/https" = "firefox.desktop";
+    "x-scheme-handler/chrome" = "firefox.desktop";
+    "application/x-extension-htm" = "firefox.desktop";
+    "application/x-extension-shtml" = "firefox.desktop";
+    "application/xhtml+xml" = "firefox.desktop";
+    "application/x-extension-xhtml" = "firefox.desktop";
+    "application/x-extension-xht" = "firefox.desktop";
+    "application/x-extension-html" = "firefox.desktop";
 
     # Images
     "image/bmp" = "org.gnome.eog.desktop";
