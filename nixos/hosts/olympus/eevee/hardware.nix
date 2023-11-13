dddcf0d8c67c76a164c3bb69ae7124acae1715df
Victor <victor@xirion.net>
2023-11-13 17:39:38 +0100

    fix lints

--- nixos/hosts/olympus/eevee/hardware.nix
+++ nixos/hosts/olympus/eevee/hardware.nix
@@ -1,4 +1,4 @@
-{ pkgs, config, ... }: {
+{ pkgs, ... }: {
   hardware = {
     enableAllFirmware = true;
     nvidia = {
--- nixos/hosts/olympus/grist/configuration.nix
+++ nixos/hosts/olympus/grist/configuration.nix
@@ -17,40 +17,42 @@ in {
   networking.firewall.allowedTCPPorts = [ 8484 ];
 
   environment.systemPackages = with pkgs; [ sqlite ];
+  virtualisation = {
 
-  virtualisation.podman.enable = true;
-  virtualisation.oci-containers.backend = "podman";
+    podman.enable = true;
+    oci-containers.backend = "podman";
+
+    oci-containers.containers.grist = {
+      image =
+        "gristlabs/grist:latest"; # Switch to versioned release as soon as OIDC hits stable
+      environment = {
+        APP_HOME_URL = "https://grist.0x76.dev";
+        GRIST_SUPPORT_ANON = "false";
+        GRIST_FORCE_LOGIN = "true";
+        GRIST_SINGLE_ORG = "xirion";
+        GRIST_ORG_IN_PATH = "false";
+        GRIST_DEFAULT_EMAIL = "v@0x76.dev"; # Defines admin user
+        GRIST_HIDE_UI_ELEMENTS =
+          "helpCenter,billing,templates,multiSite,multiAccounts";
+        GRIST_TELEMETRY_LEVEL = "off";
+        GRIST_WIDGET_LIST_URL =
+          "https://github.com/gristlabs/grist-widget/releases/download/latest/manifest.json";
+
+        # No Python 2
+        PYTHON_VERSION = "3";
+        PYTHON_VERSION_ON_CREATION = "3";
+
+        # Beta OIDC support
+        GRIST_OIDC_IDP_ISSUER = "https://dex.0x76.dev";
+      };
+      environmentFiles = [ "${vs.grist}/environment" ];
+      ports = [ "8484:8484" ];
+      volumes = [ "/var/lib/grist:/persist" ];
+    };
+  };
 
   vault-secrets.secrets.grist = {
     quoteEnvironmentValues = false; # Needed for docker
     services = [ "podman-grist" ];
-  };
-
-  virtualisation.oci-containers.containers.grist = {
-    image =
-      "gristlabs/grist:latest"; # Switch to versioned release as soon as OIDC hits stable
-    environment = {
-      APP_HOME_URL = "https://grist.0x76.dev";
-      GRIST_SUPPORT_ANON = "false";
-      GRIST_FORCE_LOGIN = "true";
-      GRIST_SINGLE_ORG = "xirion";
-      GRIST_ORG_IN_PATH = "false";
-      GRIST_DEFAULT_EMAIL = "v@0x76.dev"; # Defines admin user
-      GRIST_HIDE_UI_ELEMENTS =
-        "helpCenter,billing,templates,multiSite,multiAccounts";
-      GRIST_TELEMETRY_LEVEL = "off";
-      GRIST_WIDGET_LIST_URL =
-        "https://github.com/gristlabs/grist-widget/releases/download/latest/manifest.json";
-
-      # No Python 2
-      PYTHON_VERSION = "3";
-      PYTHON_VERSION_ON_CREATION = "3";
-
-      # Beta OIDC support
-      GRIST_OIDC_IDP_ISSUER = "https://dex.0x76.dev";
-    };
-    environmentFiles = [ "${vs.grist}/environment" ];
-    ports = [ "8484:8484" ];
-    volumes = [ "/var/lib/grist:/persist" ];
   };
 }
