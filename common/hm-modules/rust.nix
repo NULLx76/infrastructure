b7f7c8f0a328231348dec03f2d00c56725016907
Vivian <vivian@0x76.dev>
2024-03-19 10:27:04 +0100

    mold in rust

--- common/hm-modules/rust.nix
+++ common/hm-modules/rust.nix
@@ -15,6 +15,10 @@ in {
           [build]
           rustc-wrapper = "${pkgs.sccache}/bin/sccache"
 
+          [target.'cfg(not(target_arch = "mips"))']
+          linker = "${pkgs.clang}/bin/clang"
+          rustflags = ["-C", "link-arg=-fuse-ld=${pkgs.mold}/bin/mold"]
+
           [profile.rust-analyzer]
           inherits = "dev"
         '';
